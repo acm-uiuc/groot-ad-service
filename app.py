e7b496de08ce3f0157b411ce51e6b1a5f1c70ff3
engr-acm-sa-adduser <engr-acm-sa-adduser@ad.uillinois.edu>
2018-09-30 13:54:21 -0500

    changes

--- app.py
+++ app.py
@@ -7,15 +7,8 @@ from secrets import SERVICE_ACCESS_TOKEN, MYSQL
 from datetime import datetime
 from models import Users, db
 
-scheduler = BackgroundScheduler()
 app = Flask(__name__)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
-    MYSQL['user'],
-    MYSQL['password'],
-    MYSQL['host'],
-    MYSQL['dbname']
-)
+
 
 def verifyAddition(netid):
     execproc = subprocess.Popen([r'powershell.exe',
@@ -23,8 +16,6 @@ def verifyAddition(netid):
     netid], stdout=subprocess.PIPE, cwd=os.getcwd())
     out, err = execproc.communicate()
     if(out.decode().strip() == "1"):
-        Users.query.filter_by(netid=netid).update(dict(added_to_directory=True))
-        db.session.commit()
         return True
 
 def runScript(netid):
@@ -34,20 +25,13 @@ def runScript(netid):
     result = execproc.wait()
     return result
 
-@scheduler.scheduled_job('interval', days=1, next_run_time=datetime.now())
-def addUserDaily():
-    paidUsers = Users.query.filter_by(added_to_directory=False).filter_by(is_member=True).all()
-    print(paidUsers)
-    for user in paidUsers:
-        runScript(user.netid)
-        verifyAddition(user.netid)
 
 @app.route("/activedirectory/add/<string:netid>")
 def addUser(netid):
     if request.headers.get('Authorization') == SERVICE_ACCESS_TOKEN:
         output = runScript(netid)
         if(verifyAddition(netid)):
-            return make_response(jsonify(dict(message=str("Added the user.")), 200)
+            return make_response(jsonify(dict(message="Added the user.")), 200)
         else:
             return make_response(jsonify(dict(error="There was an error.")), 400)
 
@@ -58,8 +42,6 @@ def addUser(netid):
 def root():
     return make_response(jsonify(dict(message="Welcome!")), 200)
 
-db.init_app(app)
-db.create_all(app=app)
 
 if __name__ == "__main__":
     app.run(port=80, host='0.0.0.0')
